99b21b79e20c519f95ce62719c517012d029e0a0
Andrew <Andrew>
2019-10-31 00:45:35 -0400

    fixed bug with sh file.

--- castilloAPgm3/CastilloAPgm3.sh
+++ castilloAPgm3/CastilloAPgm3.sh
@@ -4,9 +4,9 @@
 #       Course    : OS2
 #       Professor : Michael Robinson 
 #       Program # : pgm3 
-#                   { A brief description of the program }
+#                   Divides by two numbers and parses hs file.
 #                
-#       #Due Date  : 03/25/2019 
+#       #Due Date  : 10/31/2019 
 #                 
 #       Certification: 
 #       I hereby certify that this work is my own 
@@ -16,7 +16,7 @@
 
 echo "Andrew Castillo `date`" >> result.txt
 echo "Division Calculator App"
-echo "Enter 99 on either first number or second number to exit application."
+echo "Enter 44 on either first number or second number to exit application when asked."
 
 while true
 
@@ -24,32 +24,43 @@ do
     echo "Enter first number: "
     read firstNumber
 
-    if [ $firstNumber -eq 99 ]; then 
+    if [ $firstNumber -eq 44 ]; then 
         break
     fi
 
     echo "Enter second number: "
     read secondNumber
 
-    if [ $secondNumber -eq 99 ];
+    if [ $secondNumber -eq 44 ];
     then
         break
     fi
     
+
     if [ $secondNumber -eq 0 ];
     then 
-        echo "You cannot divide by zero. Enter second number: "
-        read secondNumber
-        val=`expr $firstNumber / $secondNumber`
-        echo "$firstNumber divided by $secondNumber is $val"
-        echo "$firstNumber divided by $secondNumber is $val" >> result.txt
-    else
-        val=`expr $firstNumber / $secondNumber`
-        echo "$firstNumber divided by $secondNumber is $val"
-        echo "$firstNumber divided by $secondNumber is $val" >> result.txt
+    
+    while [ $secondNumber -eq 0 ]
+    do
+        echo "You cannot divide by zero. Please try again. "
+        echo "Enter second number: "
+	read secondNumber
+    done
+    fi
+
+    if [ $secondNumber -eq 44 ];
+    then
+       break
+    fi
+	
+    if [ $secondNumber -ne 0 ];
+    then
+    val=`expr $firstNumber / $secondNumber`
+    echo "$firstNumber divided by $secondNumber is $val"
+    echo "$firstNumber divided by $secondNumber is $val" >> result.txt
     fi
 
 done
 
-echo "Total amount of substring TTT in the first 2,000 lines of file hs_alt_HuRef_chr10.fa are: `head -2000 hs_alt_HuRef_chr10.fa | grep TTT -o | wc -l`"
-echo "Total amount of substring TTT in the first 2,000 lines of file hs_alt_HuRef_chr10.fa are: `head -2000 hs_alt_HuRef_chr10.fa | grep TTT -o | wc -l`" >> result.txt
+echo "Total amount of substring ACGT in the first 1,995 lines of file hs_alt_HuRef_chr10.fa are: `head -1995 hs_alt_HuRef_chr10.fa | grep ACGT -o | wc -l`"
+echo "Total amount of substring ACGT in the first 1,995 lines of file hs_alt_HuRef_chr10.fa are: `head -1995 hs_alt_HuRef_chr10.fa | grep ACGT -o | wc -l`" >> result.txt
